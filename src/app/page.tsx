05024ad113e533406b9d9b40b07ebbe05a7ca235
Brandon Griffiths <brandonjosephgriffiths@gmail.com>
2023-11-09 07:21:07 -0700

    feat: Added Meta Data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <h1 className="font-display text-5xl font-bold">My Website</h1>
+      <h1 className="font-display text-5xl font-bold">My Website!</h1>
       <p className="font-body text-5xl font-bold">Sweet</p>
     </main>
   );
